90fce8b70f0ddfcdfc245e110368340bdd409c6d
James Sherry <james.m.sherry@googlemail.com>
2025-02-24 21:20:28 +0000

    post change commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Hello
+          Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
